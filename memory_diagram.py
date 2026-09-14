9da7d3c3eced0e0d1ed6ac752765916c4c5dddbc
alice.pollock@ucas-edu.net <alice.pollock@ucas-edu.net>
2026-09-14 14:53:27 -0600

    Memory Diagram

---
+++ memory_diagram.py
@@ -0,0 +1,21 @@
+# AP, Memory Digram Activity
+
+name = "Alex"
+print(f"The variable Alex has a value of {name} and is saved at {id(name)}")
+age = 14
+print(f"The variable 14 has a value of {age} and is saved at {id(age)}")
+score = 10
+print(f"The variable 10 has a value of {score and is saved at {id(score)}")
+score = 25
+print(f"The variable 25 has a value of {score} and is saved at {id(score)}")
+height = 5.9
+print(f"The variable 5.9 has a value of {height} and is saved at {id(height)}")
+city = "Denver"
+print(f"The variable Denver has a value of {city} and is at {id(city)}")
+zip_code = 80202
+print(f"The variable 80202 has a value of {zip_code} and is saved at {id(zip_code)}")
+price1 = 10
+print(f"The variable 10 has a value of {price1} and is saved at {id(price1)}")
+price2= 10
+print(f"The variable 10 has a value of {price2} and is saved at {id(price2)}")
+
