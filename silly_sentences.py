77e9afeef421df212a9c81b2f48ee31520f3fcdf
alice.pollock@ucas-edu.net <alice.pollock@ucas-edu.net>
2026-09-21 14:44:41 -0600

    Silly sentences

---
+++ silly_sentences.py
@@ -0,0 +1,10 @@
+# AP, Silly Senctences activity
+
+verb_ing = input("Tell me a verb ending in ing:").strip().lower()
+place = input("Tell me a place:").strip().lower()
+color = input("Tell me a color:").strip().lower()
+travel = input("Tell me a way to move:").strip().lower()
+emotion = input("Tell me an emotion:").strip().lower()
+animal = input("Tell me an animal:").strip().lower()
+
+print("One day, I was "+verb_ing+" through "+place+" when I saw a huge "+color+" "+animal+"! I felt so much "+emotion+", but I decided to "+travel+" closer to it. Suddenly the animal started following me! I tried to get away, but it would not leave me alone. Finally, I made it home. What a crazy day.")
