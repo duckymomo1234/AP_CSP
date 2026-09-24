e6506972610ed8357f45559ee5598c2c475aa33f
duckymomo1234 <alice.pollock@ucas-edu.net>
2026-09-24 01:30:46 +0000

    password strength

--- password_strength.py
+++ password_strength.py
@@ -11,15 +11,23 @@ password = input("What is your password:")
 for letter in password:
     if len(password) >= 8:
         characters = True
-else:
-    characters= False
+    if len(password) < 8:
+        characters= False
 
-if letter.isupper():
-    uppercase = True
-else:
-    uppercase = False
-
-if letter.islower():
+    if letter.isupper():
+        uppercase = True
+    if letter.islower():
         lowercase = True
+    if letter.isdigit():
+        number = True
+    if letter in "!@#$%^&*()":
+        symbol = True
+
+score = characters + uppercase + lowercase + number + symbol
+
+if score == 5:
+    print("Your password is strong")
+elif score >= 3:
+    print("Your password is medium")
 else:
-    lowercase = False
+    print("Your password is weak")
