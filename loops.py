bc71527069b6d3352ab66bbb10fc9afc5b72ce6b
alice.pollock@ucas-edu.net <alice.pollock@ucas-edu.net>
2026-09-25 14:53:16 -0600

    numbers guessing game

--- loops.py
+++ loops.py
@@ -20,3 +20,24 @@ print("GOOSE!!!!")
 # Complex data type = hold other data in it
 siblings = ["Ian", "Quintin", "Violet", "Ashton"]
 print(siblings[2])
+#adding to a list
+siblings.append("Lena")      #adds to the end of a list
+siblings.insert(2, "Alice")          
+print(siblings)
+#Remove from a list
+siblings.pop()       # <= if no number given pop removes the last item
+
+#print each item in a list
+for sibling in siblings:
+    print(sibling)
+
+# For Loops
+for num in range(1,25):
+    if num % 15 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
